5aae8fa7503b1b6adbc0207f7389d14bd7b14a19
Anna <anisferraz@gmail.com>
2022-10-30 09:34:51 -0300

    update

--- bot.py
+++ bot.py
@@ -1,24 +1,47 @@
 from chatterbot import ChatBot
-from chatterbot.comparisons import levenshtein_distance
-from chatterbot.response_selection import get_first_response
+from difflib import SequenceMatcher
+
+CONFIANCA_MINIMA = 0.60
+
+def comparar_mensagens(mensagem_digitada, mensagem_candidata):
+    confianca = 0.0
+
+    digitada = mensagem_digitada.text
+    candidata = mensagem_candidata.text
+    if digitada and candidata:
+        confianca = SequenceMatcher(None, 
+            digitada,
+            candidata)
+        confianca = round(confianca.ratio(), 2)
+
+    return confianca
 
 def main(): 
-    bot = ChatBot('Robô de atendimento Aribot', logic_adapters=[
-        {
-            "import_path": "chatterbot.logic.BestMatch",
-            "statement_comparison_function": levenshtein_distance,
-            "response_selection_method": get_first_response
-        }
-    ])
-    
+    bot = ChatBot('Robô de atendimento Aribot', 
+        read_only=True,
+        statement_comparison_function=comparar_mensagens,     
+        logic_adapters=[
+            {
+                "import_path": "chatterbot.logic.BestMatch"
+            }
+        ]
+    )
+
+    return bot 
+
+def executar_bot(bot):
     while True:
-        entrada = input('Como posso ajudar? \n')
-        resposta = bot.get_response(entrada)
-        
-        if resposta.confidence > 0.6:
-            print(resposta.text)
+        mensagem = input('Como posso ajudar? \n')
+        resposta = bot.get_response(mensagem.lower())
+
+        print('confianca', resposta.confidence)
+
+        if resposta.confidence >= CONFIANCA_MINIMA:
+            print(resposta.text + "\n")
         else:
-            print('Não sei responder essa pergunta :( ')
+            print('Não sei responder essa pergunta :( \n')
                 
 if __name__ == "__main__":
-    main()
+    bot = main()
+
+    executar_bot(bot)
