3c2fe30e1154440c55e04caa10b4e814537bfeaa
Anna <anisferraz@gmail.com>
2022-11-01 16:23:25 -0300

    update serviço web

--- servico_web.py
+++ servico_web.py
@@ -8,9 +8,12 @@ servico_bot = Flask(__name__)
 
 @servico_bot.route("/resposta/<mensagem>", methods=["GET"])
 def get_resposta(mensagem):
-    resposta = bot.get_response(mensagem)
+    resposta = bot.get_response(mensagem.lower())
 
-    return resposta.text
+    if resposta.confidence >= 0.60:
+        return resposta.text
+    else:
+        return ("Não sei responder essa pergunta")
 
 if __name__ == "__main__":
     servico_bot.run()
